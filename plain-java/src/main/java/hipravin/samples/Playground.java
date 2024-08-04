10fe52e8486d79e0a9732785ab9cae2506c76bda
Alex <alexander.vmk@gmail.com>
2024-08-04 10:00:58 +0300

    test idea git stage

--- plain-java/src/main/java/hipravin/samples/Playground.java
+++ plain-java/src/main/java/hipravin/samples/Playground.java
@@ -2,16 +2,17 @@ package hipravin.samples;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.Random;
 
 public class Playground {
     public static void main(String[] args) throws IOException, URISyntaxException {
-        probe("C:\\dev\\projects\\samples\\plain-java\\target\\classes\\hipravin\\samples\\concurrency\\CompletableFutureAllofThousands.class");
-        probe("C:\\dev\\projects\\samples\\plain-java\\src\\main\\java\\hipravin\\samples\\file\\FindFilesRecursively.java");
+
+
+
+
     }
 
-    public static void probe(String path) throws IOException {
-        System.out.println(Files.probeContentType(Path.of(path)) + "\t <- " + path);
-    }
+
+
+
 }
