169896afc1a27fdabccd156d53c5e74b8694454b
Alex <alexander.vmk@gmail.com>
2024-08-05 20:48:36 +0300

    testing git commit amend amended after push

--- plain-java/src/main/java/hipravin/samples/Playground.java
+++ plain-java/src/main/java/hipravin/samples/Playground.java
@@ -2,11 +2,10 @@ package hipravin.samples;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.Random;
 
 public class Playground {
     public static void main(String[] args) throws IOException, URISyntaxException {
-
+        //some change emended after push
 
 
 
